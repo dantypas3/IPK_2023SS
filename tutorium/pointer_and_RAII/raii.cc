63e13c5e09a509b79f6137087fd8d18060fcb291
Timo Braungardt <timo.braungardt@web.de>
2023-04-12 11:10:47 +0200

    add comments

--- tutorium/pointer_and_RAII/raii.cc
+++ tutorium/pointer_and_RAII/raii.cc
@@ -13,12 +13,12 @@ class Foo{
 
 
 class Bar{
-    Foo f;
-    //Foo* f;
+    Foo f;          // allocate in Stack
+    //Foo* f;       // allocate in Heap
     public:
     Bar() {
         std::cout << "Konstruktor Bar" << std::endl;
-        //f = new Foo;          // dinge die Dynamisch allokiert werden
+        //f = new Foo;          // dinge die Dynamisch (Heap) allokiert werden
     }
 
     ~Bar() {
@@ -27,17 +27,17 @@ class Bar{
     }
 };
 
-
+// What is RAII https://stackoverflow.com/a/713773      https://en.cppreference.com/w/cpp/language/raii
 int main() {
     Bar b;
 
     // bsp. für memory leak - mit htop anschauen wie der Memory verbrauch wächst
     u_int64_t* point = nullptr;
-    for (int i = 0; i <= 999999; i++){
+    for (int i = 0; i < 999999; i++){
         point = new u_int64_t;
         std::cout << i << " " << *point << std::endl;   // man muss was mit dem Wert machen, sonst wirds rausoptimiert
     }
-
+    
     std::cout << "Drücke STRG + D";
     std::cin >> *point;
 }
